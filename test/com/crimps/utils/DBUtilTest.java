e38d6ab40c2df255f1341363b49997b32deaec21
damonMPR <woolpack.cwz@gmail.com>
2017-03-03 10:54:41 +0800

    Initial commit

---
+++ test/com/crimps/utils/DBUtilTest.java
@@ -0,0 +1,16 @@
+package com.crimps.utils;
+
+import org.junit.Test;
+
+
+/**
+ * @author crimps
+ * @version 1.0  17/3/3
+ * @modified crimps  17/3/3  <创建>
+ */
+public class DBUtilTest {
+    @Test
+    public void testConnection() throws Exception{
+        DBUtil.connection(null);
+    }
+}
